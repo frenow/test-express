020c4aea5a6a9349c31f3e83f16d9c7f17f9c846
Ubuntu <ubuntu@ip-172-31-39-242.ec2.internal>
2020-07-28 17:13:15 +0000

    add function fib

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+function fib(n) {
+	if(n <= 1) {
+		return n;
+	}
+	else {
+		return fib(n - 1) + fib(n - 2);
+	}
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Hello Express' });
+router.get('/fib', async function(req, res, next) {
+const f = await fib(40);
+res.json({ title: 'Hello Fibonacci 40', f });
 });
 
 module.exports = router;
